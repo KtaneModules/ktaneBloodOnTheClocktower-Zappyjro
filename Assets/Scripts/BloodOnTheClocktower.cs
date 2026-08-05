768a86e5b7189f6f4f0dbb5ff7a12cffa70314af
Zappyjro <zappyjro@gmail.com>
2026-08-05 17:25:27 +0100

    Further modelling and all highlights

--- Assets/Scripts/BloodOnTheClocktower.cs
+++ Assets/Scripts/BloodOnTheClocktower.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BloodOnTheClocktower : MonoBehaviour {
+	public KMAudio Audio;
+	public KMBombModule Module;
+	public KMBombInfo Info;
+	public KMModSettings modSettings;
+	public KMSelectable moduleSelectable;
+	public KMSelectable[] characterButtons;
+	public KMSelectable[] nameButtons;
+	public KMSelectable submitButton;
+	public TextMesh[] nameFields;
+	public TextMesh displayField;
+	public Material[] characterIconMaterials;
+	public KMColorblindMode ColourBlindMode;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
 }
